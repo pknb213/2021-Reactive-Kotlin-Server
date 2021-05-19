e31d3c1ff09e553816480f14f21cdb60383dfc82
Office_YJ <pknb213@naver.com>
2021-05-19 16:38:45 +0900

    Added HTTP Server
    just ping api

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,6 +1,34 @@
+import com.mongodb.connection.ClusterSettings
+import com.mongodb.reactivestreams.client.MongoClient
+import com.mongodb.reactivestreams.client.MongoClients
+import reactor.core.publisher.Mono
+import reactor.netty.DisposableServer
+import reactor.netty.http.server.HttpServer
+import reactor.netty.tcp.TcpServer
+
 object Server {
+    init {
+        val TEST_DB_NAME = "userhabit"
+        val db by lazy {
+            val client = MongoClients.create("mongodb://localhost:27017")
+            client.getDatabase(TEST_DB_NAME)
+        }
+        println(db.listCollections())
+        db.createCollection("Test_Collection")
+    }
     @JvmStatic
     fun main(args: Array<String>) {
+
+        val server = HttpServer.create()
+            .route{
+                it
+                    .get("/ping") { req, res -> res.sendString(Mono.just("Pong\n")) }
+            }
+            .port(4500)
+            .bindNow()
+
         print(" > > > Hello")
+        server.onDispose()
+            .block()
     }
 }
