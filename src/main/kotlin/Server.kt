b6faf50c21f782af4232767cc637918347509361
Office_YJ <pknb213@naver.com>
2021-05-21 18:41:05 +0900

    Error

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,46 +1,82 @@
-import com.mongodb.connection.ClusterSettings
 import com.mongodb.reactivestreams.client.MongoClient
 import com.mongodb.reactivestreams.client.MongoClients
+import io.netty.handler.codec.http.HttpHeaderNames
+import io.netty.handler.codec.http.HttpHeaderValues
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import reactor.netty.DisposableServer
+import reactor.core.scheduler.Schedulers
+import reactor.netty.NettyOutbound
 import reactor.netty.http.server.HttpServer
-import reactor.netty.tcp.TcpServer
+import reactor.netty.http.server.HttpServerRequest
+import reactor.netty.http.server.HttpServerResponse
+import kotlin.reflect.typeOf
 
 object Server {
     init {
         val TEST_DB_NAME = "userhabit"
         val TEST_COLLECTION_NAME = "app"
-//        val db by lazy {
-//            val client = MongoClients.create("mongodb://localhost:27017")
-//            client.getDatabase(TEST_DB_NAME)
-//        }
-//        println(db.listCollections())
-//        db.createCollection("Test_Collection")
-        val client = MongoClients.create()
-        println(">> ${client}")
-        val db = client.getDatabase(TEST_DB_NAME)
-        println(">> ${db}")
-        val coll = db.getCollection(TEST_COLLECTION_NAME)
-        println(">> ${coll}")
-        val collog = Mono.just(coll).log()
-        println(collog)
-        collog.subscribe({
-            println(it.toString())
-        })
 
+        val flux = Flux.just(7, 4, 11 ,3)
+            .map { it + 1 }
+            .filter { it % 2 == 0 }
+            .scan { acc, value -> acc + value }
+            .collectList()
+            .subscribeOn(Schedulers.parallel())
+            .block()
+    }
+    fun get(): Mono<String> {
+        println("Hell~")
+        val client = MongoClients.create("mongodb://localhost:27017")
+        val ls = client.listDatabases()
+        val mono = Mono
+            .from(ls)
+            .map {
+                it.map {
+                    println(it.toString())
+                }
+            }
+            .toString()
+//        val db = client.getDatabase("userhabit")
+//        val coll = db.getCollection("app")
+//        val flux = Flux
+//            .from(coll.find())
+//            .collectList()
+//            .map {
+//                println(it.toString())
+//                it.map {
+//                    println(it.toString())
+//                }
+//                it.toString()
+//            }
+
+        return Mono.just(mono.toString())
     }
     @JvmStatic
     fun main(args: Array<String>) {
-
         val server = HttpServer.create()
             .route{
                 it
                     .get("/ping") { req, res -> res.sendString(Mono.just("Pong\n")) }
+                    .get("/test") { req, res ->
+                        val client = MongoClients.create("mongodb://localhost:27017")
+                        val ls = client.listDatabases()
+//                        val mono = Mono
+//                            .from(ls)
+//                            .map {
+//                                println(it.toString())
+//                                it.toString()
+//                            }
+                        val flux = Flux
+                            .just(3,4,8)
+
+                        println(flux)
+                        res.sendString(Mono.just(flux.toString()))
+                    }
             }
             .port(4500)
             .bindNow()
 
-        print(" > > > Hello")
+        println(" > > > Hello")
         server.onDispose()
             .block()
     }
