62997917675db764fd3ae3c4c057f6dc66723bb1
CheonYoungJo <pknb213@naver.com>
2021-05-24 02:20:51 +0900

    Updated

--- src/main/kotlin/Mono_Operation.kt
+++ src/main/kotlin/Mono_Operation.kt
@@ -1,5 +1,7 @@
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.util.function.Tuple2
+
 fun `map test`() {
     Mono.just("hello")
         .map {
@@ -25,21 +27,43 @@ fun `filter test`() {
             println("$it True!")
         }
 }
+fun `zip when test`() {
+    Mono.just("hello")
+        .zipWhen {
+            Mono.just("$it world")
+        }.subscribe { it: Tuple2<String, String> ->
+            println("${it.t1}, ${it.t2}")
+        }
+}
+fun `zip with test`() {
 
+    Mono.just("hello")
+        .zipWith(Mono.just("world"))
+        .subscribe { it: Tuple2<String, String> ->
+            println("${it.t1}, ${it.t2}")
+        }
+}
+fun `concat with test`() {
+    Mono.just("hello")
+        .concatWith(Mono.just("world"))
+        .subscribe {
+            println(it)
+        }
+}
 object Mono_Operation {
     @JvmStatic
     fun main(args: Array<String>) {
-        println(">> Map")
+        println(">> map")
         `map test`()
-        println(">> FlatMap")
+        println(">> flatMap")
         `flat map test`()
-        println(">> Filter")
+        println(">> filter")
         `filter test`()
-        println(">>  ")
-        `filter test`()
-        println(">>  ")
-        `filter test`()
-        println(">>  ")
-        `filter test`()
+        println(">> zipWhen")
+        `zip when test`()
+        println(">> zipWith")
+        `zip with test`()
+        println(">> concatWith")
+        `concat with test`()
     }
 }
